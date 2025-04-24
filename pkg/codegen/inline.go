607dfe03115152193e89978e0583430a757f431b
Johan Larsson <jl311888@gmail.com>
2025-04-24 08:48:52 +0200

    load validation layer spec from embedded spec.yaml

--- pkg/codegen/inline.go
+++ pkg/codegen/inline.go
@@ -16,65 +16,27 @@ package codegen
 import (
 	"bufio"
 	"bytes"
-	"compress/gzip"
-	"encoding/base64"
 	"fmt"
 	"text/template"
 
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
-// This generates a gzipped, base64 encoded JSON representation of the
-// swagger definition, which we embed inside the generated code.
-func GenerateInlinedSpec(t *template.Template, importMapping importMap, swagger *openapi3.T) (string, error) {
-	// Marshal to json
-	encoded, err := swagger.MarshalJSON()
-	if err != nil {
-		return "", fmt.Errorf("error marshaling swagger: %s", err)
-	}
-
-	// gzip
+// Embed swagger spec.
+func GenerateEmbeddedSpec(t *template.Template, importMapping importMap, swagger *openapi3.T) (string, error) {
 	var buf bytes.Buffer
-	zw, err := gzip.NewWriterLevel(&buf, gzip.BestCompression)
-	if err != nil {
-		return "", fmt.Errorf("error creating gzip compressor: %s", err)
-	}
-	_, err = zw.Write(encoded)
-	if err != nil {
-		return "", fmt.Errorf("error gzipping swagger file: %s", err)
-	}
-	err = zw.Close()
-	if err != nil {
-		return "", fmt.Errorf("error gzipping swagger file: %s", err)
-	}
-	str := base64.StdEncoding.EncodeToString(buf.Bytes())
 
-	var parts []string
-	const width = 80
-
-	// Chop up the string into an array of strings.
-	for len(str) > width {
-		part := str[0:width]
-		parts = append(parts, part)
-		str = str[width:]
-	}
-	if len(str) > 0 {
-		parts = append(parts, str)
-	}
-
-	// Generate inline code.
 	buf.Reset()
 	w := bufio.NewWriter(&buf)
-	err = t.ExecuteTemplate(w, "inline.tmpl", struct {
-		SpecParts     []string
+	err := t.ExecuteTemplate(w, "embedded.tmpl", struct {
 		ImportMapping importMap
-	}{SpecParts: parts, ImportMapping: importMapping})
+	}{ImportMapping: importMapping})
 	if err != nil {
-		return "", fmt.Errorf("error generating inlined spec: %s", err)
+		return "", fmt.Errorf("error generating embedded spec: %s", err)
 	}
 	err = w.Flush()
 	if err != nil {
-		return "", fmt.Errorf("error flushing output buffer for inlined spec: %s", err)
+		return "", fmt.Errorf("error flushing output buffer for embedded spec: %s", err)
 	}
 	return buf.String(), nil
 }
